9fdd1242ef147cf4347ec254a0af787708f77cbc
firuzaismayilova <ismayilova.firuze2003@gmail.com>
2025-05-14 17:04:41 +0400

    initial

---
+++ script.js
@@ -0,0 +1,12 @@
+<nav class="navbar">
+    <div class="logo">BenimSite</div>
+    <div class="hamburger" id="hamburger">☰</div>
+    <ul class="nav-links" id="nav-links">
+      <li><a href="#">Anasayfa</a></li>
+      <li><a href="#">Hakkımızda</a></li>
+      <li><a href="#">Hizmetler</a></li>
+      <li><a href="#">İletişim</a></li>
+    </ul>
+  </nav>
+
+  <script src="script.js"></script>
